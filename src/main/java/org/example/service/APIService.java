40421396f6640af24d9f4c356bf5cadd97906be5
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-03-10 16:07:55 +0900

    [fix] dotenv 수정

--- src/main/java/org/example/service/APIService.java
+++ src/main/java/org/example/service/APIService.java
@@ -17,7 +17,6 @@ import java.util.Map;
 public class APIService {
     private static final APIService instance = new APIService();
     private final HttpClient httpClient = HttpClient.newHttpClient();
-    private final Dotenv dotenv = Dotenv.load();
     private final String groqToken;
     private final String togetherToken;
     private final String groqGuide;
@@ -28,6 +27,7 @@ public class APIService {
     }
 
     private APIService() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         groqToken = dotenv.get("GROQ_KEY");
         togetherToken = dotenv.get("TOGETHER_KEY");
         groqGuide = dotenv.get("GROQ_GUIDE");
